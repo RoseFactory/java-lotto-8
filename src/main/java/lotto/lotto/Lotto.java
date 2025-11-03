5933a8e6038ba995a2f12fa5fe295ca179f8911b
RoseFactory <1547892user@naver.com>
2025-11-03 13:45:39 +0900

    refactor: Lotto 생성 시 숫자 범위 검증

--- src/main/java/lotto/lotto/Lotto.java
+++ src/main/java/lotto/lotto/Lotto.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Set;
 
 public class Lotto {
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -18,14 +19,18 @@ public class Lotto {
         }
 
         Set<Integer> set = new HashSet<>(numbers);
-        if(numbers.size() != set.size()) {
+        if (numbers.size() != set.size()) {
             throw new IllegalArgumentException("[ERROR] 로또 번호에는 중복이 없어야 합니다.");
+        }
+
+        boolean numberNotInRangeOneToFortyFiveExists = numbers.stream()
+                                                        .anyMatch(n -> n < 1 || n > 45);
+        if(numberNotInRangeOneToFortyFiveExists) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1-45 사이여야 합니다.");
         }
     }
 
     public List<Integer> getNumbers() {
         return List.copyOf(numbers);
     }
-
-    // TODO: 추가 기능 구현
 }
